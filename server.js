84f87bdcae64fb2ea11643a00e6e50a18d3c3583
WellingtonSilva12 <wellingtonsilva180g@gmail.com>
2022-06-23 07:44:31 -0300

    finishing ejs classes

--- server.js
+++ server.js
@@ -5,7 +5,38 @@ const app = express()
 app.set('view engine', 'ejs')
 
 app.get('/', function (req, res) {
-  res.render('index')
+  const items = [
+    {
+      title: 'D - ',
+      message: 'Desenvolver aplicações de forma fácil'
+    },
+    {
+      title: 'E - ',
+      message: 'EJS usa Javascript'
+    },
+    {
+      title: 'M - ',
+      message: 'Manuseio com facilidade'
+    },
+    {
+      title: 'A - ',
+      message: 'Desenvolver aplicações de forma fácil'
+    },
+    {
+      title: 'I - ',
+      message: 'Desenvolver aplicações de forma fácil'
+    },
+    {
+      title: 'S - ',
+      message: 'Desenvolver aplicações de forma fácil'
+    }
+  ]
+  const subtitle =
+    'Uma linguagem de modelagem de forma fácil, que renderiza HTML em javascript'
+  res.render('index', {
+    qualitys: items,
+    subtitle: subtitle
+  })
 })
 
 app.get('/sobre', function (req, res) {
